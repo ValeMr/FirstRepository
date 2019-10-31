4b165347551a0ac44c848df9cdef3ea953e1d441
Valeria <Valeria@DESKTOP-SLG31K2>
2019-10-31 10:04:21 +0100

    traccia

---
+++ Prova/src/traccia/InvalidPlayException.java
@@ -0,0 +1,12 @@
+package traccia;
+
+public class InvalidPlayException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public InvalidPlayException(String message) {
+		super(message);
+	}
+
+	
+}
